20aacf6e2819586171a840dec7bb5bc14d3145d8
ChrisDougherty <91543335+ChrisDougherty@users.noreply.github.com>
2022-01-14 10:19:36 -0500

    Create options.js

---
+++ screens/options.js
@@ -0,0 +1,63 @@
+import React from "react";
+import {
+  SafeAreaView,
+  View,
+  Text,
+  StyleSheet,
+  TouchableHighlight,
+  Linking,
+} from "react-native";
+import { Entypo } from "@expo/vector-icons";
+import { RowSeperator, RowItem } from "../components/RowSeperator.js";
+
+const styles = StyleSheet.create({
+  text: {
+    fontSize: 45,
+    paddingHorizontal: 15,
+    margin: 15,
+  },
+  seperator: {
+    backgroundColor: "#101010",
+    height: StyleSheet.hairlineWidth,
+    marginHorizontal: 20,
+  },
+  row: {
+    alignItems: "center",
+    flexDirection: "row",
+    justifyContent: "space-between",
+    marginRight: 20,
+  },
+});
+const openURL = (url) => {
+  Linking.openURL(url).catch(alert("There was an error!"));
+};
+
+export default () => {
+  return (
+    <SafeAreaView>
+      <RowItem
+        title="My Github"
+        icon={<Entypo name="arrow-right" size={24} color="black" />}
+        onPress={() => openURL("https://chrisdougherty.github.io/")}
+      />
+
+      <RowSeperator />
+
+      <RowItem
+        title="Responsive Image"
+        icon={<Entypo name="arrow-right" size={24} color="black" />}
+        onPress={() =>
+          openURL("https://chrisdougherty.github.io/responsive/index")
+        }
+      />
+
+      <RowSeperator />
+
+      <RowItem
+        title="Restaurant"
+        icon={<Entypo name="arrow-right" size={24} color="black" />}
+        onPress={() => openURL("https://chrisdougherty.github.io/menu/index")}
+      />
+    </SafeAreaView>
+  );
+};
